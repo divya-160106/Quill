1e64bc0edd5847a032e7e0188f5810a774c8bda9
Divyasree Manikandan <divyasreem1601@gmail.com>
2026-06-13 14:34:02 +0530

    Add API_URL constant to QuillChat component

--- frontend/src/components/QuillChat.jsx
+++ frontend/src/components/QuillChat.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./../styles/QuillChat.css";
 
 import MessageBubble from "./MessageBubble";
+const API_URL = import.meta.env.VITE_API_URL;
 
 function QuillChat() {
 
